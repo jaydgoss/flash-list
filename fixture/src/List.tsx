cb336493404dbe662c147b226267ec9fecd979f8
Daniel Friyia JR <daniel.friyia@shopify.com>
2023-02-25 09:51:32 -0500

    [DF] feat(#547): `experimentalMaintainTopContentPosition` prop added to TypeScript

--- fixture/src/List.tsx
+++ fixture/src/List.tsx
@@ -127,6 +127,7 @@ const List = () => {
         //     <Text>{isLoading ? "LOADING" : ""}</Text>
         //   </View>
         // }
+        experimentalMaintainTopContentPosition
       />
     </>
   );
